ce04c1f7768c63c47dba4e1e2305c4efd5cecea2
tejeshwini2425 <133632622+tejeshwini2425@users.noreply.github.com>
2025-07-31 11:50:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var book = req.body.book;
+   
+    var id = req.body.id;
+   
+
+    var data = {
+        "Book": book,
+      
+        "Id": id,
+        
+    }
+
+    db.collection('borrow').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(9000);
+
+
+console.log("Listening on PORT 3000");
